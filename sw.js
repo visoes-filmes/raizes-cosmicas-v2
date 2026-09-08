454d9b3fdaf61ec197edc6cb2a052e14037b4dcf
Crisia-poria <admcrisia@gmail.com>
2026-09-08 15:53:05 -0300

    o solo chega ao meio, os cogumelos param de se amontoar, as pedras ganham terra

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * momento em que metade da obra é de uma versão e metade de outra.
  */
 
-const VERSAO = 'raizes-cosmicas-2026-09-08ao';
+const VERSAO = 'raizes-cosmicas-2026-09-08ap';
 
 /**
  * O que é baixado na instalação, sem esperar ninguém pedir.
