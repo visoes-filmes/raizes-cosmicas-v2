9bc387ab49404fff15f36e08913b3e4991042d5a
Crisia-poria <admcrisia@gmail.com>
2026-09-08 18:11:01 -0300

    a tinta das arvores abre: o preto volta e a cor fica presente

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * momento em que metade da obra é de uma versão e metade de outra.
  */
 
-const VERSAO = 'raizes-cosmicas-2026-09-08ap';
+const VERSAO = 'raizes-cosmicas-2026-09-08aq';
 
 /**
  * O que é baixado na instalação, sem esperar ninguém pedir.
