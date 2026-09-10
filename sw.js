4fea1c0b8768bb2dbdce38f2ff6dee9fb9044e4c
Crisia-poria <admcrisia@gmail.com>
2026-09-10 15:28:12 -0300

    a copa da arvore-mae em camadas: forquilhas escalonadas e quatro ordens de ramo no lugar do forcado

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * momento em que metade da obra é de uma versão e metade de outra.
  */
 
-const VERSAO = 'raizes-cosmicas-2026-09-10h';
+const VERSAO = 'raizes-cosmicas-2026-09-10j';
 
 /**
  * O que é baixado na instalação, sem esperar ninguém pedir.
