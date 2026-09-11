6dbc37ce6704470ad88102ad34e17a2cd8c77153
Crisia-poria <admcrisia@gmail.com>
2026-09-10 23:51:44 -0300

    a borboleta da a volta em quem olha, sobe para o infinito levando o mundo e vira a estrela que vem ser o Sol -- de ambar, no lado infinito

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * momento em que metade da obra é de uma versão e metade de outra.
  */
 
-const VERSAO = 'raizes-cosmicas-2026-09-10az';
+const VERSAO = 'raizes-cosmicas-2026-09-10bc';
 
 /**
  * O que é baixado na instalação, sem esperar ninguém pedir.
