168360530e35e46fe79c7d9b5abd94bebd3788e7
Crisia-poria <admcrisia@gmail.com>
2026-09-10 22:04:07 -0300

    o dorso da borboleta no azul-morpho medido na textura do proprio modelo

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
  * momento em que metade da obra é de uma versão e metade de outra.
  */
 
-const VERSAO = 'raizes-cosmicas-2026-09-10an';
+const VERSAO = 'raizes-cosmicas-2026-09-10ap';
 
 /**
  * O que é baixado na instalação, sem esperar ninguém pedir.
